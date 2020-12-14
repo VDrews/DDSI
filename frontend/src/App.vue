b90fd8a75caeea95547a2b2814d9082de9792416
VDrews <thedrews@outlook.es>
2020-12-14 13:31:31 +0100

    Dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <v-navigation-drawer app>
+      <div class="display-2 ma-3" style="font-weight: 800">DDSI</div>
+      <v-list-item @click="">
+        Contabilidad
+      </v-list-item>
+      <v-list-item @click="">
+        Inventario
+      </v-list-item>
+      <v-list-item @click="">
+        Logistica
+      </v-list-item>
+      <v-list-item @click="">
+        Marketing
+      </v-list-item>
+      <v-list-item @click="">
+        Recursos Humanos
+      </v-list-item>
+      
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <router-view class="ma-6"></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    
   },
 
   data: () => ({
