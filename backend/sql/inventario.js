081fea71d553822eeaa6b9a7a925bbcabab57633
VDrews <thedrews@outlook.es>
2020-12-14 12:32:49 +0100

    Añadido archivo de consultas sql

---
+++ backend/sql/inventario.js
@@ -0,0 +1,8 @@
+
+let sinVariable = `CREATE TABLE WHERE ***`
+
+let conVariable =  function(nombreVariable) {
+  `CREATE TABLE WHERE ${nombreVariable} = ***`
+}
+
+export default {sinVariable, nombreVariable}
