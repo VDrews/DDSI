d25ad345d34421794398d00588262d04fc86eb88
VDrews <thedrews@outlook.es>
2020-12-14 13:17:50 +0100

    Vuetify y MySQL instalado

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,30 @@ const express=require('express')
 const bodyParser=require('body-parser')
 
 const app=express()
-const api=require('./routes')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use('/api', api)
 
 
-
-const express=require('express')
-const app=express()
 var cors = require('cors');
-var bodyParser = require('body-parser')
 
 app.use(cors());
 app.use( bodyParser.json() ); 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', )
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'frontend',
+  password : 'DDSI2020'
+});
+
+connection.connect();
+
+// app.post('/', )
 
 // const config=require('./config')
+
+
+connection.end();
