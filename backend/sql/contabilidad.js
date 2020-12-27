99fd9d7ad57ac6d3c9f98c746fa87f4b066acbcc
Carlota Valdivia <carlotillavm99@gmail.com>
2020-12-27 10:49:14 +0100

    Probando otra vez

--- backend/sql/contabilidad.js
+++ backend/sql/contabilidad.js
@@ -1,8 +1,33 @@
+// nombre de usuario en Transacción
+// los enumerados van entre ''?
+// en el update hay que poner ; ?
+// para generar factura es necesario poder agrupar el código de la factura, el tipo, nombre de usuario y cantidad
+// entonces con eso se haría un select y se escribiría en "papel"
+// nombre usuario requisito funcional 4.2
+// y cómo seleccionamos el código de factura?!
 
-let sinVariable = `CREATE TABLE WHERE ***`
-
-let conVariable =  function(nombreVariable) {
-  `CREATE TABLE WHERE ${nombreVariable} = ***`
+let anotarIngresoGasto =  function(tipo, cantidad) {
+  return `insert into Transaccion (tipo, cantidad) values (${tipo}, ${cantidad});`
 }
 
-module.exports = {sinVariable, nombreVariable}
+// La otra opción sería buscar por el cod_transacción y cambiar el requisito f.
+let consultarIngresoGasto = function(nombre_usuario) {
+	return `select tipo, cantidad from Transaccion where codigo_tr in (select codigo_Tr from Generacion where codigo_fac in (select cod_factura from CompraVenta where ID_paquete in (select ID from Envio where nombre_usuario = '${nombre_usuario}')))  `
+} 
+
+let modificarIngresoGasto = function(codigo_tr, tipo, cantidad) {
+	return `update Transaccion set tipo = ${tipo}, cantidad = ${cantidad} where codigo_tr = ${codigo_tr};`
+}
+
+// select?
+let generarFactura = function(codigo_tr) {
+	return (`insert into Generacion (codigo_tr) values (${codigo_tr});`; `select tipo, cantidad from Transacción where codigo_tr = ${codigo_tr}`)
+}
+
+
+
+`select tipo, cantidad from Transacción where codigo_tr = ${codigo_tr}`
+
+
+
+module.exports = {anotarIngresoGasto, consultarIngresoGasto, modificarIngresoGasto}
--- backend/sql/rrhh.js
+++ backend/sql/rrhh.js
@@ -1,8 +1,34 @@
 
-let sinVariable = `CREATE TABLE WHERE ***`
+let contratarEmpleado = function({dni, nombre, apellidos, sueldo}) {
+	return (`insert into Empleado (dni, nombre, apellidos) values (${dni}, '${nombre}', '${apellidos}');`; `insert into Contrato (dni, numero, turno, fecha, vigente, sueldo) values (${dni}, , , Now(), true, ${sueldo});`)
 
-let conVariable =  function(nombreVariable) {
-  `CREATE TABLE WHERE ${nombreVariable} = ***`
 }
 
-module.exports = {sinVariable, nombreVariable}
+
+
+/*
+let darBajaEmpleado = function({dni}) {
+	return `insert into (select dni, vigente from Contrato where dni = ${dni}) and vigente = false (vigente) values (false);`
+}*/
+
+let darBajaEmpleado = function({dni}) {
+	return `update Contrato set vigente = false where dni = ${dni};`
+}
+
+
+// DUDAS: número de contrato, hay que buscar que sea vigente??
+// cuando no es un string hay que poner '${}' o solo ${}
+// los true/False hay que ponerlos con comillas?
+// Como poner en contratar el número
+// Como poner en contratar el turno y la fecha
+
+let modificarEmpleado = function({dni, nombre, apellidos, sueldo, numero}) {
+	return (`update Empleado set nombre = '${nombre}', apellidos = '${apellidos} where dni = ${dni};`;  `update Contrato set sueldo = ${sueldo} where (dni = ${dni} and numero = ${numero} and vigente = true);`)
+}
+
+
+let consultarEmpleado = function({dni}) {
+	return `select dni, nombre, apellidos, numero, vigente, sueldo from Empleado NATURAL JOIN Contrato where dni = ${dni}`
+}
+
+module.exports = {contratarEmpleado, darBajaEmpleado, modificarEmpleado, consultarEmpleado}
