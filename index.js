75fa6a3e4c0ae79e98f8ced17b0cf7e52f4c2345
VDrews <thedrews@outlook.es>
2020-12-29 11:58:17 +0100

    db handleDisconnect

--- index.js
+++ index.js
@@ -44,9 +44,27 @@ var mysql = require('mysql');
 //   password: 'f53dce90',
 //   database: 'heroku_1e1951efc954bab'
 // });
-var connection = mysql.createConnection('mysql://b93f80375031dd:f53dce90@eu-cdbr-west-03.cleardb.net/heroku_1e1951efc954bab?reconnect=true');
+var connection
+function handleDisconnect() {
+  connection = mysql.createConnection('mysql://b93f80375031dd:f53dce90@eu-cdbr-west-03.cleardb.net/heroku_1e1951efc954bab?reconnect=true');
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
-connection.connect();
+handleDisconnect();
 
 const random_EAN = () => {
   min = 0
@@ -677,15 +695,6 @@ app.post('/api/logistica/compra', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Backend funcionando en http://localhost:${port}`)
-})
-
-connection.on('error', function(err) {
-  console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      connection = mysql.createConnection('mysql://b93f80375031dd:f53dce90@eu-cdbr-west-03.cleardb.net/heroku_1e1951efc954bab?reconnect=true');                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
 })
 
 process.on('error', function (err) {
